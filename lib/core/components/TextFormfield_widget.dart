870e058d877ce3d63c7ca800b3bf165576ab77a1
Ghaith <ghayth.1900@gmail.com>
2025-09-05 17:23:56 +0200

    working  on some  components

--- lib/core/components/TextFormfield_widget.dart
+++ lib/core/components/TextFormfield_widget.dart
@@ -1,18 +1,31 @@
 import 'package:ecom_app/core/app_colors.dart';
 import 'package:flutter/material.dart';
 
-class TextformfieldCustomWidget extends StatelessWidget {
+class TextformfieldCustomWidget extends StatefulWidget {
   const TextformfieldCustomWidget({
     super.key,
     required this.hintText,
+      this.isboscureText = false, this.eyeIcon,
   });
 
   final String hintText;
+  final bool isboscureText;
+  final IconData? eyeIcon;
+
+  @override
+  State<TextformfieldCustomWidget> createState() => _TextformfieldCustomWidgetState();
+}
+
+class _TextformfieldCustomWidgetState extends State<TextformfieldCustomWidget> {
+  bool isboscureText = true;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      obscureText:isboscureText ,
       decoration: InputDecoration(
-          hintText: hintText,
+        
+          hintText: widget.hintText,
+          suffixIcon: isboscureText? IconButton(onPressed: (){}, icon: icon) ,
           border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(8),
               borderSide: BorderSide(
