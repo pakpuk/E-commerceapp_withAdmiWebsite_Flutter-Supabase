ca2856e407cccd2daf641fc8e2b1c8898783ec1f
Ghaith <ghayth.1900@gmail.com>
2025-09-12 17:59:29 +0200

    finish  the  signup and  forgot  password  screens

---
+++ lib/views/Ui/signup_screen.dart
@@ -0,0 +1,202 @@
+import 'package:ecom_app/core/app_colors.dart';
+import 'package:ecom_app/core/components/TextFormfield_widget.dart';
+import 'package:ecom_app/core/text_manager.dart';
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  SignupScreen({super.key});
+  TextEditingController passwordcontroller = TextEditingController();
+  TextEditingController confirmpasswordcontroller = TextEditingController();
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController namecontroller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                TextManager.loginTitle,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Card(
+                margin: const EdgeInsets.all(24),
+                color: AppColors.kWhiteColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      TextformfieldCustomWidget(
+                        hintText: "Full Name",
+                        ispassword: false,
+                        labeltText: "Full Name",
+                        controller: namecontroller,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextformfieldCustomWidget(
+                        hintText: TextManager.emailhint,
+                        ispassword: false,
+                        labeltText: TextManager.email,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Email is required';
+                          }
+                          final emailRegex = RegExp(
+                              r'^[^@\s]+@[^@\s]+\.[^@\s]+$'); // simple email regex
+                          if (!emailRegex.hasMatch(value)) {
+                            return 'Enter a valid email';
+                          }
+                          return null;
+                        },
+                        controller: emailcontroller,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextformfieldCustomWidget(
+                        hintText: TextManager.passwordhint,
+                        ispassword: true,
+                        labeltText: TextManager.password,
+                        controller: passwordcontroller,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'This is a required field';
+                          }
+                          if (value.length < 8) {
+                            return "pleas enter a valid password";
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextformfieldCustomWidget(
+                        hintText: TextManager.confirmPasswordhint,
+                        ispassword: true,
+                        labeltText: TextManager.confirmPasswordhint,
+                        controller: confirmpasswordcontroller,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'This is a required field';
+                          }
+                          if (value.length < 8) {
+                            return "pleas enter a valid password";
+                          } else if (value != passwordcontroller.text) {
+                            return "Password does  not  match ";
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      Align(
+                          alignment: Alignment.centerRight,
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Text(
+                              TextManager.forgetpassword,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: AppColors.kPrimaryColor,
+                                fontSize: 18,
+                              ),
+                            ),
+                          )),
+                      const SizedBox(
+                        height: 18,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(TextManager.login),
+                          InkWell(
+                            onTap: () {},
+                            child: Container(
+                              height: 40,
+                              width: 60,
+                              decoration: BoxDecoration(
+                                color: AppColors.kPrimaryColor,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(12),
+                                ),
+                              ),
+                              child: Center(
+                                child: Icon(
+                                  Icons.arrow_right_alt,
+                                  color: AppColors.kWhiteColor,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(TextManager.googlelogin),
+                          InkWell(
+                            onTap: () {},
+                            child: Container(
+                              height: 40,
+                              width: 60,
+                              decoration: BoxDecoration(
+                                color: AppColors.kPrimaryColor,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(12),
+                                ),
+                              ),
+                              child: Center(
+                                child: Icon(
+                                  Icons.arrow_right_alt,
+                                  color: AppColors.kWhiteColor,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      RichText(
+                        text: TextSpan(
+                            text: TextManager.donthaveacc,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                            children: [
+                              TextSpan(
+                                  text: TextManager.signup,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      color: AppColors.kPrimaryColor))
+                            ]),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
