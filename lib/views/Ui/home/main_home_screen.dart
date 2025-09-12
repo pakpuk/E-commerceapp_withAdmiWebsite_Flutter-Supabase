0abf5843b145f936c778520564e74ce58e17f039
Ghaith <ghayth.1900@gmail.com>
2025-09-12 19:22:15 +0200

    start working  on the main design

---
+++ lib/views/Ui/home/main_home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MainHomeScreen extends StatelessWidget {
+  const MainHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.all(8),
+        child: Container(),
+      )),
+    );
+  }
+}
