dfb28b726ae4d8451ec3fc3a2a66da20dbdbf64e
Ghaith <ghayth.1900@gmail.com>
2025-09-13 01:58:55 +0200

    add cubit

---
+++ lib/views/logic/cubit/nav_bar_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'nav_bar_state.dart';
+
+class NavBarCubit extends Cubit<NavBarState> {
+  NavBarCubit() : super(NavBarInitial());
+}
