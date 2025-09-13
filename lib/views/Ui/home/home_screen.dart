0a48989a93757b55e652efe23740f3fd924f0614
Ghaith <ghayth.1900@gmail.com>
2025-09-13 03:35:38 +0200

    workin on home screen

--- lib/views/Ui/home/home_screen.dart
+++ lib/views/Ui/home/home_screen.dart
@@ -1,12 +1,45 @@
+import 'package:ecom_app/core/app_colors.dart';
+
+import 'package:ecom_app/core/text_manager.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
+  HomeScreen({super.key});
+  final TextEditingController fieldcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [],
+    return ListView(
+      children: [
+        TextField(
+            controller: fieldcontroller,
+            decoration: InputDecoration(
+              hintText: TextManager.searchbartxt,
+              suffixIcon: Container(
+                decoration: BoxDecoration(
+                    color: AppColors.kPrimaryColor,
+                    borderRadius: BorderRadius.circular(12)),
+                child: const Center(
+                    child: Icon(Icons.search_rounded,
+                        color: AppColors.kWhiteColor)),
+              ),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: AppColors.kBordersideColor,
+                  )),
+              enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: AppColors.kBordersideColor,
+                  )),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: AppColors.kPrimaryColor,
+                    width: 2,
+                  )),
+            ))
+      ],
     );
   }
 }
