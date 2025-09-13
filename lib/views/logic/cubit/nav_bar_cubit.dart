4f7b83e5d27fff5a3be655b7dca9ada34e9a8663
Ghaith <ghayth.1900@gmail.com>
2025-09-13 02:05:44 +0200

    add cubit

--- lib/views/logic/cubit/nav_bar_cubit.dart
+++ lib/views/logic/cubit/nav_bar_cubit.dart
@@ -5,4 +5,11 @@ part 'nav_bar_state.dart';
 
 class NavBarCubit extends Cubit<NavBarState> {
   NavBarCubit() : super(NavBarInitial());
+
+  int currentindex = 0;
+
+  void changeIndex(int index) {
+    currentindex = index;
+    emit(IndexChanged());
+  }
 }
--- lib/views/logic/cubit/nav_bar_state.dart
+++ lib/views/logic/cubit/nav_bar_state.dart
@@ -4,3 +4,5 @@ part of 'nav_bar_cubit.dart';
 sealed class NavBarState {}
 
 final class NavBarInitial extends NavBarState {}
+
+final class IndexChanged extends NavBarState {}
