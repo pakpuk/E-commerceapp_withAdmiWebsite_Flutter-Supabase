b478b2bce75000e63ee740b6a1144054bd477500
Ghaith <ghayth.1900@gmail.com>
2025-09-14 20:57:47 +0200

    add cubit  favorite state

--- lib/core/components/button_widget.dart
+++ lib/core/components/button_widget.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
 class CustomBtnWidget extends StatelessWidget {
-  const CustomBtnWidget({super.key});
+  CustomBtnWidget(
+      {super.key,
+      required this.width,
+      required this.backgroundColor,
+      required this.onTap,
+      required this.title});
+  final double width;
+  final Color backgroundColor;
+  final VoidCallback onTap;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 72,
-      width: double.infinity,
-      decoration: BoxDecoration(),
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 72,
+        width: width,
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+        ),
+        child: Center(
+          child: Text(title),
+        ),
+      ),
     );
   }
 }
