7a3ae3ac890fe24e8de2338f4ee1f0dad6c4699e
Ghaith <ghayth.1900@gmail.com>
2025-09-19 08:13:38 +0200

    finish profile screen

--- lib/views/Ui/profile_screen.dart
+++ lib/views/Ui/profile_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ecom_app/core/app_colors.dart';
+import 'package:ecom_app/core/components/profile_option.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -5,6 +7,56 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Column(
+        children: [
+          Spacer(),
+          Card(
+            color: AppColors.kWhiteColor,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+              Radius.circular(12),
+            )),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  const CircleAvatar(
+                    backgroundColor: AppColors.kPrimaryColor,
+                    radius: 32,
+                    child: Icon(
+                      Icons.person,
+                      color: AppColors.kWhiteColor,
+                      size: 52,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  ProfileOptionWidget(
+                      onTap: () {},
+                      icon: Icons.person,
+                      optiooonTitle: 'Edit Name'),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  ProfileOptionWidget(
+                      onTap: () {},
+                      icon: Icons.shop,
+                      optiooonTitle: 'My Orders'),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  ProfileOptionWidget(
+                      onTap: () {},
+                      icon: Icons.person,
+                      optiooonTitle: 'Log out'),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
