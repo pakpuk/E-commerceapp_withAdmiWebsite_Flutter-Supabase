4bc9a14b5c3801d0ebc87b5eab994462b611972d
Ghaith <ghayth.1900@gmail.com>
2025-09-19 09:40:28 +0200

    finish profile screen

--- lib/views/Ui/profile_screen.dart
+++ lib/views/Ui/profile_screen.dart
@@ -10,56 +10,60 @@ class ProfileScreen extends StatelessWidget {
     return SafeArea(
       child: Column(
         children: [
-          Spacer(),
-          Card(
-            color: AppColors.kWhiteColor,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-              Radius.circular(12),
-            )),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  const CircleAvatar(
-                    backgroundColor: AppColors.kPrimaryColor,
-                    radius: 32,
-                    child: Icon(
-                      Icons.person,
-                      color: AppColors.kWhiteColor,
-                      size: 52,
-                    ),
+          Center(
+            child: SizedBox(
+              height: MediaQuery.sizeOf(context).height * 0.5,
+              child: Card(
+                color: AppColors.kWhiteColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                  Radius.circular(12),
+                )),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      const CircleAvatar(
+                        backgroundColor: AppColors.kPrimaryColor,
+                        radius: 60,
+                        child: Icon(
+                          Icons.person,
+                          color: AppColors.kWhiteColor,
+                          size: 52,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text("Mohammed"),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text("user@gmail.com"),
+                      const SizedBox(
+                        height: 18,
+                      ),
+                      ProfileOptionWidget(
+                          onTap: () {},
+                          icon: Icons.person,
+                          optiooonTitle: 'Edit Name'),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      ProfileOptionWidget(
+                          onTap: () {},
+                          icon: Icons.shopping_bag,
+                          optiooonTitle: 'My Orders'),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      ProfileOptionWidget(
+                          onTap: () {},
+                          icon: Icons.person,
+                          optiooonTitle: 'Log out'),
+                    ],
                   ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Text("Mohammed"),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text("user@gmail.com"),
-                  const SizedBox(
-                    height: 18,
-                  ),
-                  ProfileOptionWidget(
-                      onTap: () {},
-                      icon: Icons.person,
-                      optiooonTitle: 'Edit Name'),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  ProfileOptionWidget(
-                      onTap: () {},
-                      icon: Icons.shop,
-                      optiooonTitle: 'My Orders'),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  ProfileOptionWidget(
-                      onTap: () {},
-                      icon: Icons.person,
-                      optiooonTitle: 'Log out'),
-                ],
+                ),
               ),
             ),
           )
