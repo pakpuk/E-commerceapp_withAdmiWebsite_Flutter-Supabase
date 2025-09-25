d6c67b77323aa7c0c66f8e250f124044d198fcdb
Ghaith <ghayth.1900@gmail.com>
2025-09-25 13:15:02 +0200

    working  on  product details screen

---
+++ lib/views/Ui/product_details.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailsScreen extends StatelessWidget {
+  const ProductDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
